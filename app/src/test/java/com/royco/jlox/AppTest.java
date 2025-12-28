e13c9651a47a37cb0fdbef54228ea960f95f5734
Royco <roy.cohen.il@gmail.com>
2025-12-28 21:44:24 +0200

    added the parser code for lox, chapter 4 in the book

--- app/src/test/java/com/royco/jlox/AppTest.java
+++ app/src/test/java/com/royco/jlox/AppTest.java
@@ -4,11 +4,12 @@
 package com.royco.jlox;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+
+    @Test
+    void appHasAGreeting() {
+        Lox classUnderTest = new Lox();
+        //assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
 }
